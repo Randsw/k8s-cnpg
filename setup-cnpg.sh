7705c75c0411ce883a6b9c136dfeca93d50acca3
Randsw <randsw@mail.ru>
2026-04-27 09:39:44 +0300

    Initial commit

---
+++ setup-cnpg.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+helm upgrade --install --wait --timeout 35m --atomic --namespace cnpg --create-namespace  \
+  --repo https://cloudnative-pg.github.io/charts cnpg cnpg/cloudnative-pg --values - <<EOF
+monitoring:
+  podMonitorEnabled: true
+  grafanaDashboard:
+    create: true
+EOF
+
+kubectl create namespace app || true
+
+kubectl create secret generic app-user-creds -n app \
+  --from-literal=username=app_user \
+  --from-literal=password=MySecurePassword123
+
+cat << EOF | kubectl apply -f -
+apiVersion: postgresql.cnpg.io/v1
+kind: Cluster
+metadata:
+  name: my-postgres-cluster
+  namespace: app
+spec:
+  instances: 3
+  bootstrap:
+    initdb:
+      database: app_db
+      owner: app_user
+      secret:
+        name: app-user-creds
+  storage:
+    size: 1Gi
+EOF
---
+++ setup-vms.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+set -e
+
+kubectl create namespace victoria-metrics || true
+
+helm upgrade --install --wait --timeout 35m --atomic --namespace victoria-metrics \
+  --repo https://prometheus-community.github.io/helm-charts prometheus-crd prometheus-operator-crds
+
+helm upgrade --install --wait --timeout 35m --atomic --namespace victoria-metrics --create-namespace  \
+  --repo https://victoriametrics.github.io/helm-charts vm victoria-metrics-k8s-stack --values - <<EOF
+victoria-metrics-operator:
+  createCRD: true
+vmsingle:
+  enabled: true
+  ingress:
+    enabled: true
+    hosts:
+      - single.kind.cluster
+    ingressClassName: istio
+vmagent:
+  ingress:
+    enabled: true
+    hosts:
+      - agent.kind.cluster
+    ingressClassName: istio
+alertmanager:
+  enabled: true
+  ingress:
+    enabled: true
+    ingressClassName: istio
+    hosts:
+      - alertmanager.kind.cluster
+grafana:
+  enabled: true
+  sidecar:
+    datasources:
+      searchNamespace: ALL
+    dashboards:
+      searchNamespace: ALL
+  ingress:
+    enabled: true
+    ingressClassName: istio
+    hosts:
+        - grafana.kind.cluster
+  plugins:
+    - victoriametrics-metrics-datasource
+kubeEtcd:
+  enabled: true
+  service:
+    # -- Enable service for ETCD metrics scraping
+    enabled: true
+    # -- ETCD service port
+    port: 2381
+    # -- ETCD service target port
+    targetPort: 2381
+    # -- ETCD service pods selector
+    selector:
+      component: etcd
+  vmScrape:
+    enabled: true
+    # spec for VMServiceScrape crd 
+    # https://github.com/VictoriaMetrics/operator/blob/master/docs/api.MD#vmservicescrapespec
+    spec:
+      jobLabel: jobLabel
+      endpoints:
+        - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
+          # bearerTokenSecret:
+          #   key: ""
+          port: http-metrics
+          scheme: http
+          tlsConfig:
+            caFile: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+            insecureSkipVerify: true
+kubeScheduler:
+  enabled: true
+  vmScrape:
+    enabled: true
+    # spec for VMServiceScrape crd 
+    # https://github.com/VictoriaMetrics/operator/blob/master/docs/api.MD#vmservicescrapespec
+    spec:
+      jobLabel: jobLabel
+      endpoints:
+        - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
+          # bearerTokenSecret:
+          #   key: ""
+          port: http-metrics
+          scheme: https
+          tlsConfig:
+            caFile: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+            insecureSkipVerify: true
+kubeControllerManager:
+  enabled: true
+  vmScrape:
+    enabled: true
+    # spec for VMServiceScrape crd 
+    # https://github.com/VictoriaMetrics/operator/blob/master/docs/api.MD#vmservicescrapespec
+    spec:
+      jobLabel: jobLabel
+      endpoints:
+        - bearerTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
+          # bearerTokenSecret:
+          #   key: ""
+          port: http-metrics
+          scheme: https
+          tlsConfig:
+            caFile: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+            insecureSkipVerify: true
+            serverName: kubernetes
+EOF
